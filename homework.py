135c8230862ba6f8c908fa1ea1384fe6fc289f90
Daehyun Kim <bigdarkgold@gmail.com>
2023-03-17 14:08:34 +0900

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,12 +1,6 @@
 # studying opensource_sw Classes
 # 202000449 컴퓨터전자시스템공학부 김대현
 
-# Touched by Daehyun
-# I'm 2nd Grade student
-# airplane
-
 print("My Hobby is Playing Baseball!")
 print("I want go to home..")
 print("I'm so sleepy")
-
-print("hahahahahahah")
